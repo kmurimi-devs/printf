048550b18a0d60570a729b6a33b44ab1063a4a00
kmurimi-devs <kmurimi-devs@gmail.com>
2023-07-05 05:44:22 -0700

    prototype fix

--- main.h
+++ main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+
+#define BUFF_SIZE 1024
+
 int _printf(const char *format, ...);
+int get_flags(const char *format, int *index);
+int get_width(const char *format, int *index, va_list list);
+int get_precision(const char *format, int *index, va_list list);
+int get_size(const char *format, int *index);
+int handle_print(const char *format, int *index, va_list list, char buffer[],
+		int flags, int width, int precision, int size);
 
 #endif
