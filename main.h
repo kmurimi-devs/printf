b05e3b1a07418290564f483a3fc064eb882b466a
kmurimi-devs <kmurimi-devs@gmail.com>
2023-07-05 13:04:19 -0700

    fixes

--- main.h
+++ main.h
@@ -90,7 +90,7 @@ int write_pointer(char buffer[], int ind, int length,
 
 int write_unsgnd(int is_negative, int ind, char buffer[],
 	int flags, int width, int precision, int size);
-nt is_printable(char);
+int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
 long int convert_size_number(long int num, int size);
